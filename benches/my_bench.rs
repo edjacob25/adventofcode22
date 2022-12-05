ab1996c3d5ad2b5efc98f22937c5c18e6ee4b60e
Jacob Rivera <edjacob25@gmail.com>
2022-12-05 16:10:33 -0600

    Create benches

---
+++ benches/my_bench.rs
@@ -0,0 +1,22 @@
+use advent22::problems::*;
+use criterion::{criterion_group, criterion_main, Criterion};
+
+macro_rules! bench_day {
+    ($i:ident, $day:literal, $day_path:ident) => {
+        let mut day = $i.benchmark_group(concat!("Day ", $day));
+        day.bench_function("1", |b| b.iter(|| $day_path::part1()));
+        day.bench_function("2", |b| b.iter(|| $day_path::part2()));
+        day.finish();
+    };
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    bench_day!(c, "01", day1);
+    bench_day!(c, "02", day2);
+    bench_day!(c, "03", day3);
+    bench_day!(c, "04", day4);
+    bench_day!(c, "05", day5);
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
