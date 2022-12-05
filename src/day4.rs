a4952ebca29fd10f7e5381bb2f1b57a4d990f768
Jacob Rivera <edjacob25@gmail.com>
2022-12-05 13:23:00 -0600

    Added day 4

---
+++ src/day4.rs
@@ -0,0 +1,47 @@
+use super::common::read_file;
+use std::collections::HashSet;
+
+fn convert_into_pairs(line: &str) -> Vec<(u32, u32)> {
+    let mut pairs = Vec::new();
+    for (i, p) in line.split(",").enumerate() {
+        let mut sp = p.split("-");
+        let initial = sp.next().unwrap().parse::<u32>().unwrap();
+        let fina = sp.next().unwrap().parse::<u32>().unwrap();
+        pairs.push((initial, fina));
+    }
+    pairs
+}
+
+pub fn part1() -> u32 {
+    read_file("test_files/4.txt")
+        .lines()
+        .map(|l| {
+            let pairs = convert_into_pairs(l);
+            return if (pairs[0].0 >= pairs[1].0 && pairs[0].1 <= pairs[1].1)
+                || (pairs[1].0 >= pairs[0].0 && pairs[1].1 <= pairs[0].1)
+            {
+                1
+            } else {
+                0
+            };
+        })
+        .sum()
+}
+
+pub fn part2() -> u32 {
+    read_file("test_files/4.txt")
+        .lines()
+        .map(|l| {
+            let pairs = convert_into_pairs(l);
+            return if (pairs[0].0 >= pairs[1].0 && pairs[0].1 <= pairs[1].1)
+                || (pairs[1].0 >= pairs[0].0 && pairs[1].1 <= pairs[0].1)
+                || (pairs[0].0 >= pairs[1].0 && pairs[0].0 <= pairs[1].1)
+                || (pairs[0].1 >= pairs[1].0 && pairs[0].1 <= pairs[1].1)
+            {
+                1
+            } else {
+                0
+            };
+        })
+        .sum()
+}
